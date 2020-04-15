749d53131efbeecb1b157cef4c7017e65b7a7bb8
Alex Bersenev <aleksandrbers@gmail.com>
2020-04-15 23:09:55 +0300

    Добавьте файлы проекта.

---
+++ Ping/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace Ping
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button3 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(9, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(275, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(290, 11);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Выбрать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.openfile);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(290, 45);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Проверять";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.check);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(9, 75);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(356, 95);
+            this.listBox1.TabIndex = 3;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(9, 176);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(275, 23);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(290, 176);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Экспорт";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.export);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "csv files (*.csv)|*.csv|All files (*.*)|*.*";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(96, 45);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(188, 20);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.Text = "10";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Задержка, сек";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.linkLabel1.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.linkLabel1.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.linkLabel1.LinkColor = System.Drawing.SystemColors.Highlight;
+            this.linkLabel1.Location = new System.Drawing.Point(84, 202);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(55, 13);
+            this.linkLabel1.TabIndex = 8;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Disshell";
+            this.linkLabel1.Click += new System.EventHandler(this.linkLabel1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.Control;
+            this.label2.Font = new System.Drawing.Font("Consolas", 8F);
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label2.Location = new System.Drawing.Point(8, 202);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Developed by";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(374, 219);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Batch PING";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Ping/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+using System.Threading.Tasks;
+
+namespace Ping
+{
+    public partial class Form1 : Form
+    {
+        string pathfile;
+        int delay;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void check(object sender, EventArgs e)
+        {
+            try
+            {
+                delay = int.Parse(textBox2.Text) * 1000;
+                if (delay > 0)
+                {
+                    delay = int.Parse(textBox2.Text) * 1000;
+                } else
+                {
+                    throw new Exception();
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Задержка не верна, установлено значение по умолчанию", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                textBox2.Text = 10.ToString();
+                delay = int.Parse(textBox2.Text) * 1000;
+            }
+            FileStream fs;
+            StreamReader sr;
+            List<String> ip = null;
+            try
+            {
+                fs = new FileStream(pathfile, FileMode.Open);
+                sr = new StreamReader(fs);
+                ip = new List<string>();
+                while (!sr.EndOfStream)
+                {
+                    ip.Add(sr.ReadLine());
+                }
+                sr.Dispose();
+                fs.Dispose();
+                progressBar1.Value = 0;
+                progressBar1.Maximum = ip.Count;
+                button1.Enabled = false;
+                button2.Enabled = false;
+                button3.Enabled = false;
+                var list = new List<Task>();
+                foreach (var item in ip)
+                {
+                    list.Add(SendPing(item));
+                }
+                await Task.WhenAll(list);
+                MessageBox.Show("Выполнено успешно", "Готово", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                button1.Enabled = true;
+                button2.Enabled = true;
+                button3.Enabled = true;
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("Путь не верен", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                
+            }
+
+            
+
+        }
+
+        async private Task SendPing(string host)
+        {
+            listBox1.Items.Clear();
+            System.Net.NetworkInformation.Ping ping = new System.Net.NetworkInformation.Ping();
+            try
+            {
+                PingReply pr = await ping.SendPingAsync(host, delay);
+                listBox1.Items.Add(host + ";" +  pr.Status);
+                progressBar1.Value++;
+            }
+            catch (PingException)
+            {
+                listBox1.Items.Add(host + ";" + "Unknown Host");
+            }
+            
+        }
+
+        private void export(object sender, EventArgs e)
+        {
+            saveFileDialog1.FileName = "export.csv";
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                FileStream fs;
+                StreamWriter sw;
+                try
+                {
+                    fs = new FileStream(saveFileDialog1.FileName, FileMode.Create);
+                    sw = new StreamWriter(fs);
+                }
+                catch (Exception exc)
+                {
+                    throw exc;
+                }
+                foreach (var item in listBox1.Items)
+                {
+                    sw.Write(item.ToString()+"\n");
+                }
+
+                sw.Dispose();
+                fs.Dispose();
+            }
+        }
+
+        private void openfile(object sender, EventArgs e)
+        {
+            openFileDialog1.FileName = null;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pathfile = openFileDialog1.FileName;
+                textBox1.Text = pathfile;
+            }
+        }
+
+        private void linkLabel1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/Disshell");
+
+        }
+    }
+}
